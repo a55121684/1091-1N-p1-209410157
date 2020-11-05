9f65cb3057cfc6751a1971c40f627ecfd65eca91
Aa55121648 <209410157@gms.tku.edu.tw>
2020-11-05 22:30:48 +0800

    HomeWork

---
+++ W7/nav.js
@@ -0,0 +1,11 @@
+function change(index) {
+    const p = document.getElementById('show');
+    console.log('show', p);
+    switch(index){
+        case 1:
+            break;
+            case 2:
+    }
+    p.innerHTML='<iframe width=100% src="./w01/pokemon.html" />';
+    
+}
